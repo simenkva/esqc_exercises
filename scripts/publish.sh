517648320d17e4ea479b774114d1d67cc5610a49
simenkva <simen.kvaal@gmail.com>
2026-08-21 12:27:08 +0200

    workaround quarto multiformat bug

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -eu
 
-quarto render --profile student
-quarto render --profile tutor
+for profile in student tutor; do
+    quarto render --profile "$profile" --to html
+    quarto render --profile "$profile" --to epub --no-clean
+    quarto render --profile "$profile" --to pdf --no-clean
+done
 git add -A
 git diff --cached --quiet || git commit -m "render $(date '+%F %T')"
 git pull --rebase
