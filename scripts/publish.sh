943891024f50173fa3bc753d0fcadabc8342ab30
simenkva <simen.kvaal@gmail.com>
2026-09-07 15:32:52 +0200

    render 2026-09-07 15:32:52

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,7 +6,7 @@ for profile in student tutor; do
 #for profile in tutor; do
     quarto render --profile "$profile" --to html
 #    quarto render --profile "$profile" --to epub --no-clean
-    quarto render --profile "$profile" --to pdf --no-clean
+#    quarto render --profile "$profile" --to pdf --no-clean
 done
 git add -A
 git diff --cached --quiet || git commit -m "render $(date '+%F %T')"
